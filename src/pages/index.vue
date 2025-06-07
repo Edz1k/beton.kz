2f8a100deda0dc489aea88ee042d957e46c07670
Eduard <shartner2006@mail.ru>
2025-06-07 15:19:16 +0500

    1 stage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,24 +6,27 @@ defineOptions({
 
 <template>
   <section class="bg-[url('/background.png')] flex min-h-screen items-center relative bg-cover bg-center">
-    <div class="mx-auto container">
-      <!-- Затемнение -->
-      <div class="bg-black/60 inset-0 absolute z-0" />
+    <!-- Затемнение -->
+    <div class="bg-black/60 inset-0 absolute z-0" />
 
-      <!-- Контент -->
-      <div class="text-white px-4 py-12 w-1/2 relative z-10">
-        <h1 class="text-6xl font-bold">
-          Производство бетона в Казахстане
-        </h1>
-        <p class="text-lg mt-4">
-          TOO «MG Бетон» предлагает высококачественные бетонные решения и материалы по всему Казахстану. Мы производим и поставляем бетон, керамзит и строительные растворы, обеспечивая ваш проект надежностью и долговечностью.
-        </p>
-        <div class="mt-8 relative z-10">
-          <RequestModal />
+    <!-- Контейнер -->
+    <div class="mx-auto px-4 relative z-10 container">
+      <div class="text-white py-12 flex flex-col min-h-[70vh] items-start justify-center md:min-h-[auto] md:items-start">
+        <div class="text-center w-full space-y-6 md:text-left md:w-1/2">
+          <h1 class="text-4xl leading-tight font-bold md:text-6xl sm:text-5xl">
+            Производство бетона в Казахстане
+          </h1>
+          <p class="text-base sm:text-lg">
+            TOO «MG Бетон» предлагает высококачественные бетонные решения и материалы по всему Казахстану. Мы производим и поставляем бетон, керамзит и строительные растворы, обеспечивая ваш проект надежностью и долговечностью.
+          </p>
+          <div class="mt-6">
+            <RequestModal />
+          </div>
         </div>
       </div>
     </div>
   </section>
+
   <section>
     <CalcTabs />
   </section>
