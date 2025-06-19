7a3c5fee43bec15c1529190f6ea9bcb8d1ed9029
Eduard <shartner2006@mail.ru>
2025-06-20 01:31:29 +0500

    ready

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createHead } from '@vueuse/head'
+import { ViteSSG } from 'vite-ssg'
 import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
-
 import './styles/main.css'
 import 'uno.css'
 
-const app = createApp(App)
-const router = createRouter({
-  routes,
-  history: createWebHistory(import.meta.env.BASE_URL),
-})
-app.use(router)
-app.mount('#app')
+export const createApp = ViteSSG(
+  App,
+  {
+    routes,
+    base: import.meta.env.BASE_URL,
+  },
+  (ctx) => {
+    const head = createHead()
+    ctx.app.use(head)
+  },
+)
